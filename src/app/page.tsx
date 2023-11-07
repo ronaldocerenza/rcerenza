8d09cb64b60900df6a55cbac693a15ba6cdcafcc
Ronaldo Cerenza <rockronald@gmail.com>
2023-11-06 21:51:06 -0400

    feat: initial html and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
-import Image from 'next/image'
+import { Header } from './components/header/header'
+import './styles/home.scss'
 
 export default function Home() {
   return (
-    <main>
-      <Image
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
+    <main className="conteiner">
+      <Header />
+      <div className="experience">
+        <h3>Experiência</h3>
+        <p>Colocar texto</p>
+      </div>
+      <div className="experience-time">
+
+      </div>
+      <div className="infos">
+        <h3>Linguagens</h3>
+        <div className="linguages-info">
+          <span>🇧🇷 PT-BR - Nativo</span>
+          <span>🇪🇸 ES - Intermediário</span>
+          <div className="Education">
+            <span>🎓 UEMS - Economia</span>
+            <span>🎓 Laureat - Docencia Superior</span>
+            <span>🧠 Trybe - FullStack</span>
+            <span>🎓 ITI - Cientista de Dados</span>
+          </div>
+        </div>
+        <div className="buttons">
+            <div className="social">
+
+            </div>
+            <button>Entre em Contato</button>
+        </div>
+      </div>
     </main>
   )
 }
